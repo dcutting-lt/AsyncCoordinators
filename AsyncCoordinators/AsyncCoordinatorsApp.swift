aabbcd614b9e860f82a4b52000ecbc1d85c3572b
Dan Cutting <dcutting@lightricks.com>
2021-08-23 16:18:10 +1000

    Initial Commit

---
+++ AsyncCoordinators/AsyncCoordinatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncCoordinatorsApp.swift
+//  AsyncCoordinators
+//
+//  Created by Dan Cutting on 23/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncCoordinatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncCoordinators/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncCoordinators
+//
+//  Created by Dan Cutting on 23/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
